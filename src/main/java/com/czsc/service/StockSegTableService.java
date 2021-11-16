c9eec678f131f3d2984d5d79479ec81ca98ea82e
nrahbshiqt <946963646@qq.com>
2021-11-16 17:06:37 +0800

    init file

--- src/main/java/com/czsc/service/StockSegTableService.java
+++
@@ -1,8 +0,0 @@
-package com.czsc.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.czsc.entity.StockBiTable;
-import com.czsc.entity.StockSegTable;
-
-public interface StockSegTableService extends IService<StockSegTable> {
-}
