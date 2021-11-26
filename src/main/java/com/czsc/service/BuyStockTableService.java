58d350c0ef01f4e95ddd1d4d7c87508155760fef
nrahbshiqt <946963646@qq.com>
2021-11-26 22:05:41 +0800

    init file

--- src/main/java/com/czsc/service/BuyStockTableService.java
+++
@@ -1,8 +0,0 @@
-package com.czsc.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.czsc.entity.BuyStockTable;
-
-
-public interface BuyStockTableService extends IService<BuyStockTable> {
-}
--- src/main/java/com/czsc/service/StockPivotTableService.java
+++
@@ -1,7 +0,0 @@
-package com.czsc.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.czsc.entity.StockPivotTable;
-
-public interface StockPivotTableService extends IService<StockPivotTable> {
-}
